81130bf3c24291c61c1b2b7993f0bdca28ec99c2
yoxcu <mick@yoxcu.de>
2022-10-14 22:08:39 +0200

    new name

--- sendmail_example.sh
+++
@@ -1,8 +0,0 @@
-swaks --from john.smith@mydomain.com \
---h-From: '"John Smith" <john.smith@mydomain.com>' \
---h-Subject: 'Subject of message' \
---auth LOGIN --auth-user mylogin --auth-pass mypass \
---to someone@otherdomain.com \
---server smtp.example.com --port 25 -tls \
---add-header 'Content-Type: text/plain; charset="utf-8"' \
---body "$1"
